b86c0049f21485444df78a631497d2daa1dd9550
Facundo Lobo <facundolobo2@gmail.com>
2024-10-01 23:04:38 -0300

    BD con Web

--- db/conexion.js
+++ db/conexion.js
@@ -2,10 +2,12 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.NOMBREBBDD, process.env.USERBBDD, process.env.PASSWORD,{
-    host: process.env.DIRECCIONBBDD,
-    dialect: 'mysql'
-}) 
+const db = new Sequelize(process.env.DIRECCIONBBDDWEB) // Example for postgres
+
+// const db = new Sequelize(process.env.NOMBREBBDD, process.env.USERBBDD, process.env.PASSWORD,{
+//     host: process.env.DIRECCIONBBDD,
+//     dialect: 'mysql'
+// }) 
 
 
 module.exports = {
